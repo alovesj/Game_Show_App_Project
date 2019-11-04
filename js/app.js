5d3e727db29f316963f07d0aac21ec25c8942cea
AngieC <alovesj@hotmail.com>
2019-11-04 13:30:41 -0600

    added favicon, get random phrase function

--- js/app.js
+++ js/app.js
@@ -2,52 +2,57 @@ const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const missed = 0;
 const div = document.getElementById('overlay');
-const ul = document.getElementById('phrase');
-
+const ul = document.querySelector('ul');
 const li = document.createElement('li');
 
-// quotes from John Maxwell and Dave Ramsey
+
+// quotes from John Maxwell and Dave Ramsey, me
 var phrases = ['There is no substitute for hard work',
               'Creativity requires the willingness to look stupid',
-              'We cannot lead anyone farther than we have been ourselves',
+              'No Change No Gain',
               'Act your wage',
               'You have to tell your money what to do or it will leave',
-              'You cant be a smart cookie if you have a crummy attitude'
+              'You cant be a smart cookie if you have a crummy attitude',
             ];
 
 
 div.addEventListener('click', (e) => {
   div.style.display = "none";
-  console.log(phrases);
+
 });
 
-var randomNumber = Math.floor( Math.random() * 6 ) + 1;
-var phraseToGuess = phrases[randomNumber];
-var charactersToGuess = Array.from(phraseToGuess);
+// var charactersToGuess = [];
 
-function getRandomPhraseAsArray(phrases) {
-
-  console.log(phrases[randomNumber]);
-  console.log(charactersToGuess,'');
-  return charactersToGuess;
+function getRandomPhraseAsArray(arr) {
+  let randomNumber = Math.floor(Math.random() * 5) +1;
+  console.log(randomNumber);
+  let phraseToGuess = arr[randomNumber];
+  let charactersToGuess = Array.from(phraseToGuess);
+  //var charactersToGuess = phraseToGuess.split();
+    console.log(phrases[randomNumber]);
+    console.log(charactersToGuess);
+    return charactersToGuess;
 };
+
 getRandomPhraseAsArray(phrases);
 
 
-// const arr = Array.from(getRandomPhraseAsArray(phrases));
-//
-// function addPhraseToDisplay(arr) {
-//   for (let i=0; i<= arr.length; i++) {
-//
-//   } const letter = arr[i];
-//     ul.appendChild(li);
-//     if (letter != ' ' ){
-//       li.className = 'letter';
-//
-//
-//     }
-// };
-// addPhraseToDisplay(charactersToGuess);
+function addPhraseToDisplay(arr) {
+  //loop over array
+  for (let i = 0; i <= arr.length; i++) {
+  //put each letter in a var as it loops
+      const letters = arr[i];
+      const li = document.createElement('li');
 
-//call function
-getRandomPhraseAsArray(phrases);
+      console.log(letters);
+      ul.appendChild(li);
+
+  }
+
+    // if (li != ' ' ){
+    //   li.className = 'letter';
+
+
+    // }
+};
+addPhraseToDisplay(phrases);
