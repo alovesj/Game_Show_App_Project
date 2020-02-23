53f830d77ac0a9d31fa85e5ecdadb46abd535572
AngieC <alovesj@hotmail.com>
2020-02-22 20:48:14 -0600

    added 'chosen' class to matching and missed letters when selected

--- js/app.js
+++ js/app.js
@@ -1,83 +1,115 @@
-const qwertydiv = document.querySelector('#qwerty');
-const phrase = document.querySelector('#phrase');
-const missed = 0;
-const div = document.getElementById('overlay');
-const ul = document.querySelector('ul');
-const button = document.querySelector('button');
+document.addEventListener('DOMContentLoaded', () => {
 
 
-// quotes from John Maxwell and Dave Ramsey, me
-var phrases = ['There is no substitute for hard work',
-              'Creativity requires the willingness to look stupid',
-              'No Change No Gain',
-              'Act your wage',
-              'You have to tell your money what to do or it will leave',
-              'You cant be a smart cookie if you have a crummy attitude',
-            ];
+  const qwertydiv = document.querySelector('#qwerty');
+  const phrase = document.querySelector('#phrase');
+  var missed = 0;
+  const startButton = document.getElementById('overlay');
+  const ul = document.querySelector('ul');
+  const button = document.querySelector('button');
 
 
-div.addEventListener('click', (e) => {
-  div.style.display = "none";
-});
+  // quotes from John Maxwell and Dave Ramsey, me
+  var phrases = ['There is no substitute for hard work',
+    'Creativity requires the willingness to look stupid',
+    'No Change No Gain',
+    'Act your wage',
+    'You have to tell your money what to do or it will leave',
+    'You cant be a smart cookie if you have a crummy attitude',
+  ];
 
-function getRandomPhraseAsArray(arr) {
-  let phraseToGuess = arr[Math.floor(Math.random() * arr.length)];
+
+  startButton.addEventListener('click', (e) => {
+    startButton.style.display = "none";
+  });
+
+  function getRandomPhraseAsArray(arr) {
+    let phraseToGuess = arr[Math.floor(Math.random() * arr.length)];
     console.log(phraseToGuess);
     return phraseToGuess.split('');
 
-};
+  };
 
-let charactersToGuess = getRandomPhraseAsArray(phrases);
+  let charactersToGuess = getRandomPhraseAsArray(phrases);
 
 
-function addPhraseToDisplay(arr) {
-  //loop over array
-  for (let i = 0; i < arr.length; i ++) {
-  //put each letter in a var as it loops
+  function addPhraseToDisplay(arr) {
+    //loop over array
+    for (let i = 0; i < arr.length; i++) {
+      //put each letter in a var as it loops
       const li = document.createElement('li');
       li.textContent = arr[i];
       console.log(li);
       ul.appendChild(li);
-      if(li.textContent == " ") {
+      if (li.textContent == " ") {
         li.className = 'space';
       } else {
         li.className = 'letter';
-    }
-  }
-};
-addPhraseToDisplay(charactersToGuess);
-
-
-qwertydiv.addEventListener('click', (e) => {
-
-  function checkLetter(button) {
-    //get all letters in random phrase
-    const letters = document.querySelectorAll('.letter');
-    var match = 0;
-    var letter = 0;
-    for (let i = 0; i < letters.length; i++) {
-      //get button's letter
-      //compare button against phrase
-      var letter = letters[i].textContent;
-      console.log(letter);
-      console.log(letter.toLowerCase());
-      if (letter.toLowerCase() === e.target.textContent) {
-        console.log('match');
-        var match = e.target.textContent;
-        letters[i].classList.add('show');
-        //return match;
-
-      }else {
-        //missed += 1;
-
-        console.log('null');
-
       }
-
-    } return match;
-
+    }
   };
+  addPhraseToDisplay(charactersToGuess);
 
-  checkLetter();
+
+  qwertydiv.addEventListener('click', (e) => {
+
+    if (e.target.classList.contains('chosen')){
+      alert('already chosen');
+      return;
+    };
+
+
+    var match = 0;
+    const tries = document.querySelector('ol');
+    const li = tries.childNode;
+
+    function checkLetter(button) {
+
+
+      const letters = document.querySelectorAll('.letter');
+
+      var letter = 0;
+      for (let i = 0; i < letters.length; i++) {
+        //get button's letter
+        //compare button against phrase
+        var letter = letters[i].textContent;
+        console.log(letter);
+        console.log(letter.toLowerCase());
+        if (letter.toLowerCase() === e.target.textContent) {
+          console.log('match');
+          var match = e.target.textContent;
+          console.log(match);
+          letters[i].classList.add('show');
+
+        } else {
+          // return 'null';
+          console.log('null');
+          console.log(missed);
+
+        }
+      } e.target.classList.add('chosen');
+      return match;
+      missed++
+    };
+    checkLetter();
 
 });
+
+
+  // if (e.target.classlist === 'chosen') {
+  //   alert("letter already chosen");
+  //}
+  //get all letters in random phrase
+
+
+  //var checked =
+
+  // if (checked === 'null') {
+  //   tries.remove.childNode;
+  //   ++missed;
+  // }
+
+});
+//When I click on a letter not in phrase, I need it to count up 1 miss
+
+//and mark letter as already attempted.
